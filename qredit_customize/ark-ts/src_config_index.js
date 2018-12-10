6a861b3e61ba7444361aab689f5cc6dae68bf792
nayiem <info@hodlercompany.com>
2018-12-10 20:20:22 +0100

    edit for qredit

---
+++ qredit_customize/ark-ts/src_config_index.js
@@ -0,0 +1,34 @@
+"use strict";
+// tslint:disable:object-literal-sort-keys
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    networks: {
+        mainnet: {
+            bip32: {
+                private: 0x4358394,
+                public: 0x43587cf,
+            },
+            name: 'Qredit',
+            nethash: '5e67037fd290ba7ab378e84a591d251c46eb9770eb134983771fd602233bf193',
+            token: 'Qredit',
+            symbol: 'XQR',
+            version: 0x3a,
+            explorer: 'http://185.85.18.192:4200',
+            wif: 0xbb,
+            p2pVersion: '1.1.0',
+            activePeer: {
+                ip: '185.85.18.192',
+                port: 4100,
+            },
+            peers: [
+                '136.144.215.24:4101'
+            ],
+        },
+    },
+    blockchain: {
+        interval: 8,
+        delegates: 51,
+        date: new Date(Date.UTC(2017, 2, 21, 13, 0, 0, 0)),
+    },
+};
+//# sourceMappingURL=index.js.map
